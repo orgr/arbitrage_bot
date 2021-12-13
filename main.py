09f9c4c076a547b9e3b16a2d314b733da2e3b08c
Grinberg, Or <or.grin@gmail.com>
2021-12-13 06:58:04 +0200

    make loading markets & fetching prices async

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import sys
 import time
 from typing import List
 
-import ccxt
+import asyncio
+import ccxt.async_support as ccxt
+# import ccxt
 import itertools
 
 from enum import Enum
@@ -16,7 +18,8 @@ class Color(Enum):
 
 
 def colorize(s, color: Color):
-    return color.value + s + Color.RESET.value
+    # return color.value + s + Color.RESET.value
+    return "{}{}{}".format(color.value, s, Color.RESET.value)
 
 
 def green(s):
@@ -41,7 +44,7 @@ class ArbitrageOpportunity(Enum):
 
 
 def get_complementary_trade(t: ArbitrageOpportunity):
-    assert(t != ArbitrageOpportunity.NONE)
+    assert (t != ArbitrageOpportunity.NONE)
     return ArbitrageOpportunity.BUY if t == ArbitrageOpportunity.SELL else ArbitrageOpportunity.SELL
 
 
@@ -66,52 +69,69 @@ def compare_prices(p1: Price, p2: Price):
     return p1.is_opportunity(p2)
 
 
-def main():
+async def get_price(symbol, exchange) -> Price:
+    orderbook = await exchange.fetch_order_book(symbol, 10)
+    bid = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
+    ask = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
+    # spread = (ask - bid) if (bid and ask) else None
+    # print(ex.id, 'market price', {'bid': bid, 'ask': ask, 'spread': spread})
+    if bid is None or ask is None:
+        return None
+
+    return Price(exchange, symbol, float(bid), float(ask))
+
+
+async def main():
     if len(sys.argv) < 3:
         print("Usage: python {} <delta threshold> <exchange id 1> <exchange id 2> ...".format(sys.argv[0]))
         return
     exchanges = []
 
-    # initialize exchanges
-    for ex_id in sys.argv[1:]:
-        try:
-            ex = getattr(ccxt, ex_id)({'enableRateLimit': True})  # type: ccxt.Exchange
-            ex.load_markets()
-            # ex.set_sandbox_mode(enabled=True)
-        except AttributeError:
-            print("{} is not supported".format(ex_id))
-            return
-        except ccxt.NotSupported:
-            print("{} paper trading is not supported".format(ex_id))
-            return
-        exchanges.append(ex)
+    try:
+        # initialize exchanges
+        tasks = []
+        for ex_id in sys.argv[1:]:
+            try:
+                ex = getattr(ccxt, ex_id)({'enableRateLimit': True})  # type: ccxt.Exchange
+                # ex.set_sandbox_mode(enabled=True)
+            except AttributeError:
+                print("{} is not supported".format(ex_id))
+                return
+            except ccxt.NotSupported:
+                print("{} paper trading is not supported".format(ex_id))
+                return
 
-    all_symbols = [symbol for ex in exchanges for symbol in ex.symbols]
-    unique_arbitrable_symbols = set([symbol for symbol in all_symbols if all_symbols.count(symbol) > 1])
+            tasks.append(asyncio.create_task(ex.load_markets()))
+            exchanges.append(ex)
 
-    for symbol in unique_arbitrable_symbols:
-        prices: List[Price] = []
-        for ex in exchanges:
-            orderbook = ex.fetch_order_book(symbol, 10)
-            # time.sleep(2)
-            bid = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
-            ask = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
-            # spread = (ask - bid) if (bid and ask) else None
-            # print(ex.id, 'market price', {'bid': bid, 'ask': ask, 'spread': spread})
-            if (bid != None and ask != None):
-                prices.append(Price(ex, symbol, float(bid), float(ask)))
+        [await t for t in tasks]
 
-        if len(prices) > 1:
-            arbitrage_pairs = itertools.combinations(prices, r=2)
-            for p in arbitrage_pairs:
-                opportunity = compare_prices(p[0], p[1])
-                if opportunity != ArbitrageOpportunity.NONE:
-                    print(green(symbol + ' ' + str(opportunity) + ' from ' + p[1].exchange.id + ' '
-                                + str(get_complementary_trade(opportunity)) + ' ' + p[0].exchange.id))
-                else:
-                    print(yellow(symbol))
+        all_symbols = [symbol for ex in exchanges for symbol in ex.symbols]
+        unique_arbitrable_symbols = set([symbol for symbol in all_symbols if all_symbols.count(symbol) > 1])
+
+        for symbol in unique_arbitrable_symbols:
+            tasks = []
+            for ex in exchanges:
+                tasks.append(asyncio.create_task(get_price(symbol, ex)))
+
+            [await t for t in tasks]
+            prices = [t.result() for t in tasks]
+
+            if len(prices) > 1:
+                arbitrage_pairs = itertools.combinations(prices, r=2)
+                for p in arbitrage_pairs:
+                    opportunity = compare_prices(p[0], p[1])
+                    if opportunity != ArbitrageOpportunity.NONE:
+                        print(green("{}: {} from {}, {} from {}".format(symbol, opportunity, p[1].exchange.id,
+                                                                        get_complementary_trade(opportunity),
+                                                                        p[0].exchange.id)))
+                    else:
+                        print(yellow(symbol))
+
+    # close all connections on KeyboardInterrupts and errors
+    finally:
+        [await ex.close() for ex in exchanges]
 
 
 if __name__ == '__main__':
-    main()
-
+    asyncio.run(main())
