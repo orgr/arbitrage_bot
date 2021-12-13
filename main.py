2564f59b57374ffb200d508e01c0788e9fc769b1
Grinberg, Or <or.grin@gmail.com>
2021-12-13 05:45:03 +0200

    colorize output + get rid of delta threshold

--- main.py
+++ main.py
@@ -5,17 +5,47 @@ from typing import List
 import ccxt
 import itertools
 
-import enum
+from enum import Enum
 
 
-class ArbitrageOpportunity(enum.Enum):
+class Color(Enum):
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    RESET = '\033[0m'
+
+
+def colorize(s, color: Color):
+    return color.value + s + Color.RESET.value
+
+
+def green(s):
+    return colorize(s, Color.GREEN)
+
+
+def yellow(s):
+    return colorize(s, Color.YELLOW)
+
+
+def red(s):
+    return colorize(s, Color.RED)
+
+
+class ArbitrageOpportunity(Enum):
     NONE = 0
     BUY = 1
     SELL = 2
 
+    def __str__(self):
+        return self.name
+
+
+def get_complementary_trade(t: ArbitrageOpportunity):
+    assert(t != ArbitrageOpportunity.NONE)
+    return ArbitrageOpportunity.BUY if t == ArbitrageOpportunity.SELL else ArbitrageOpportunity.SELL
+
 
 class Price:
-    delta_threshold = 0
 
     def __init__(self, exchange, symbol, bid, ask):
         self.exchange = exchange
@@ -24,12 +54,16 @@ class Price:
         self.ask = ask
 
     def is_opportunity(self, other):
-        if self.bid + Price.delta_threshold > other.ask:
+        if self.bid > other.ask:
             return ArbitrageOpportunity.BUY  # buy from other exchange
-        if self.ask + Price.delta_threshold < other.bid:
+        if self.ask < other.bid:
             return ArbitrageOpportunity.SELL  # buy from this exchange
 
         return ArbitrageOpportunity.NONE
+
+
+def compare_prices(p1: Price, p2: Price):
+    return p1.is_opportunity(p2)
 
 
 def main():
@@ -37,10 +71,9 @@ def main():
         print("Usage: python {} <delta threshold> <exchange id 1> <exchange id 2> ...".format(sys.argv[0]))
         return
     exchanges = []
-    Price.delta_threshold = int(sys.argv[1])
 
     # initialize exchanges
-    for ex_id in sys.argv[2:]:
+    for ex_id in sys.argv[1:]:
         try:
             ex = getattr(ccxt, ex_id)({'enableRateLimit': True})  # type: ccxt.Exchange
             ex.load_markets()
@@ -56,25 +89,27 @@ def main():
     all_symbols = [symbol for ex in exchanges for symbol in ex.symbols]
     unique_arbitrable_symbols = set([symbol for symbol in all_symbols if all_symbols.count(symbol) > 1])
 
-    count = 0
     for symbol in unique_arbitrable_symbols:
-        print(symbol)
         prices: List[Price] = []
         for ex in exchanges:
             orderbook = ex.fetch_order_book(symbol, 10)
             # time.sleep(2)
             bid = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
             ask = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
-            spread = (ask - bid) if (bid and ask) else None
-            print(ex.id, 'market price', {'bid': bid, 'ask': ask, 'spread': spread})
-            prices.append(Price(ex, symbol, float(bid), float(ask)))
+            # spread = (ask - bid) if (bid and ask) else None
+            # print(ex.id, 'market price', {'bid': bid, 'ask': ask, 'spread': spread})
+            if (bid != None and ask != None):
+                prices.append(Price(ex, symbol, float(bid), float(ask)))
 
-        arbitrage_pairs = itertools.combinations(prices, r=2)
-        for p in arbitrage_pairs:
-            print(p[0].is_opportunity(p[1]))
-        count += 1
-        if count == 2:
-            break
+        if len(prices) > 1:
+            arbitrage_pairs = itertools.combinations(prices, r=2)
+            for p in arbitrage_pairs:
+                opportunity = compare_prices(p[0], p[1])
+                if opportunity != ArbitrageOpportunity.NONE:
+                    print(green(symbol + ' ' + str(opportunity) + ' from ' + p[1].exchange.id + ' '
+                                + str(get_complementary_trade(opportunity)) + ' ' + p[0].exchange.id))
+                else:
+                    print(yellow(symbol))
 
 
 if __name__ == '__main__':
